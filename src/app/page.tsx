eba217f557405c821add611b057e94c551b2833d
Adrian Stasiv <adrian.stasiv@otakoyi.com>
2025-02-23 18:36:19 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import CreateRecipe from "./_components/CreateItemRecipe";
 import { getTodo } from "./actions/recipes";
 
 export default async function Home() {
-  console.log("1234");
-
   const response = await getTodo();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
