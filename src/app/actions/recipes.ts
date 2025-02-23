b7b3ca6055a7b4fbdd206a10d6b90255f4ced5df
Adrian Stasiv <adrian.stasiv@otakoyi.com>
2025-02-23 19:16:28 +0200

    test

--- src/app/actions/recipes.ts
+++ src/app/actions/recipes.ts
@@ -5,12 +5,9 @@ import { revalidatePath } from "next/cache";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getTodo() {
-  const data = await fetch(
-    "https://recipe-app-two-psi.vercel.app/api/recipes",
-    {
-      next: { revalidate: 3600 },
-    }
-  );
+  const data = await fetch(`${API_URL}/api/recipes`, {
+    next: { revalidate: 3600 },
+  });
   const response = await data.json();
   return response;
 }
@@ -19,7 +16,7 @@ export async function createTodo(formDataValue: FormData) {
   const title = formDataValue.get("title")?.toString() || "";
   const description = formDataValue.get("description")?.toString() || "";
 
-  await fetch("https://recipe-app-two-psi.vercel.app/api/recipes", {
+  await fetch(`${API_URL}/api/recipes`, {
     method: "POST",
     body: JSON.stringify({ title, description }),
     headers: { "Content-Type": "application/json" },
