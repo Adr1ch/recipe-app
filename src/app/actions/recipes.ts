d746a55eddadc43d6e1b5a5e738e5c05175110c8
Adrian Stasiv <adrian.stasiv@otakoyi.com>
2025-02-23 19:12:28 +0200

    test

--- src/app/actions/recipes.ts
+++ src/app/actions/recipes.ts
@@ -5,9 +5,12 @@ import { revalidatePath } from "next/cache";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getTodo() {
-  const data = await fetch("/api/recipes", {
-    next: { revalidate: 3600 },
-  });
+  const data = await fetch(
+    "https://recipe-app-two-psi.vercel.app/api/recipes",
+    {
+      next: { revalidate: 3600 },
+    }
+  );
   const response = await data.json();
   return response;
 }
@@ -16,7 +19,7 @@ export async function createTodo(formDataValue: FormData) {
   const title = formDataValue.get("title")?.toString() || "";
   const description = formDataValue.get("description")?.toString() || "";
 
-  await fetch("/api/recipes", {
+  await fetch("https://recipe-app-two-psi.vercel.app/api/recipes", {
     method: "POST",
     body: JSON.stringify({ title, description }),
     headers: { "Content-Type": "application/json" },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import RecipeList from "./_components/RecipeList";
 import CreateRecipe from "./_components/CreateItemRecipe";
 import { getTodo } from "./actions/recipes";
 
-export default function Home() {
-  // const response = await getTodo();
+export default async function Home() {
+  const response = await getTodo();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex w-full flex-col gap-8 row-start-2 items-center sm:items-start">
         <CreateRecipe />
-        {/* <RecipeList recipes={response} /> */}
+        <RecipeList recipes={response} />
       </main>
     </div>
   );
