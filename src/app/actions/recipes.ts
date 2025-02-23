3bac90fc6546d53e1e161feb8a07f22968e3fb02
Adrian Stasiv <adrian.stasiv@otakoyi.com>
2025-02-23 19:07:07 +0200

    test

--- src/app/actions/recipes.ts
+++ src/app/actions/recipes.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = "https://recipe-app-two-psi.vercel.app";
 
 export async function getTodo() {
   const data = await fetch(`${API_URL}/api/recipes`, {
